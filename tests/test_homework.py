f68e08d9b47b4e9c0283ac00bb65e64437241134
Felipe Devia <EXSSafdevia@ServiciosNutresa.onmicrosoft.com>
2025-12-06 13:54:40 -0500

    prueba taller

--- tests/test_homework.py
+++ tests/test_homework.py
@@ -2,6 +2,7 @@
 
 import os
 import subprocess
+import sys
 import warnings
 
 warnings.filterwarnings("ignore")
@@ -13,7 +14,7 @@ def test_01():
     try:
         for model in ["elasticnet", "knn"]:
             subprocess.run(
-                ["python3", "-m", "homework", "--model", model],
+                [sys.executable, "-m", "homework", "--model", model],
                 check=True,
             )
     except subprocess.CalledProcessError as e:
